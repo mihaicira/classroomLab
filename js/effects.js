3bbcac6686333e59b75e1e04dd75ffada3b53edf
mihaicira <ciramihai291@gmail.com>
2021-06-30 16:34:21 +0300

    first commit

---
+++ js/effects.js
@@ -0,0 +1,46 @@
+function redirect(link){
+    window.open(link);
+}
+url = window.location.href
+if(url.includes("Activities") || url.includes("Outputs")){
+    if(url.includes("activity_") || url.includes("output_")){
+        if(url.includes("activity_"))
+            var id = "#activity_"+url[url.indexOf("activity_")+"activity_".length];
+        else
+            var id ="#output_"+url[url.indexOf("output_")+"output_".length];
+        $(id).css({
+            border: "5px solid rgb(255,0,22)"
+        });
+        setTimeout(function(){
+            $(id).css({
+                border: "5px solid transparent"
+            });
+            setTimeout(function(){
+                $(id).css({
+                    border: "5px solid rgb(255,0,22)"
+                });
+                setTimeout(function(){
+                    $(id).css({
+                        border: "5px solid transparent"
+                    });
+                },500)
+            },500)
+        }, 500);
+    }
+}
+function calcFooter(){
+    var page_content = $("#page-content").offset().top+$("#page-content").height();
+    var windowh = $(document).height();
+    var footer = $("#footer").height();
+    var margintop = (windowh-page_content) - footer;
+    console.log(margintop)
+    $("#footer").css({
+        "margin-top":""+margintop+"px"
+    });
+}
+
+calcFooter();
+$(window).on('resize',function(){
+    calcFooter();
+});
+
